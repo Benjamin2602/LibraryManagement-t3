8183bb21af51cbc1be2f6824415c0afc4e03d86e
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2024-02-20 09:25:19 +0530

    🚧 feat : Prisma , bookCard

---
+++ src/components/forms/BookCard.tsx
@@ -0,0 +1,149 @@
+"use client";
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+export function BookCard() {
+  const formSchema = z.object({
+    title: z.string(),
+    author: z.string(),
+    subject: z.string(),
+    publishedDate: z.string(),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      author: "",
+      subject: "",
+      publishedDate: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <Card className="mx-auto mt-10 w-[550px]">
+      <CardHeader>
+        <CardTitle>Add Books</CardTitle>
+        <CardDescription>Books which you like </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Title" {...field} />
+                  </FormControl>
+                  <FormDescription></FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="author"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Author</FormLabel>
+                  <FormControl>
+                    <Input placeholder="author" {...field} />
+                  </FormControl>
+                  <FormDescription></FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="subject"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="flex flex-col space-y-1.5">
+                      <Label htmlFor="framework">Genre</Label>
+                      <Select onValueChange={field.onChange}>
+                        <SelectTrigger id="framework">
+                          <SelectValue
+                            placeholder="select the genre"
+                            {...field}
+                          />
+                        </SelectTrigger>
+                        <SelectContent position="popper">
+                          <SelectItem value="motivation">Motivation</SelectItem>
+                          <SelectItem value="love">Love</SelectItem>
+                          <SelectItem value="horror">Horror</SelectItem>
+                          <SelectItem value="comedy">Comedy</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </div>
+                  </FormControl>
+                  <FormDescription></FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="publishedDate"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Published Date</FormLabel>
+                  <FormControl>
+                    <Input placeholder="date published" {...field} />
+                  </FormControl>
+                  <FormDescription></FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button type="submit" className="bg-indigo-500">
+          Add Books
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
